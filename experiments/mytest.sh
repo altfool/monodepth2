27b1acc46cc4d216db06d87d03bcad61e93dd88c
Zongming Yang <zongmiy@clemson.edu>
2020-09-04 05:09:43 -0400

    stack videos

--- experiments/mytest.sh
+++ experiments/mytest.sh
@@ -17,7 +17,15 @@ if [ ! -d "$myImgDir" ]
 then
   mkdir $myImgDir
   ffmpeg -i "$1" -f image2 "$myImgDir/"video-frame%05d.jpg
+  echo "===============video converted to frames================="
 fi
 
 cd ~/monodepth2
-python test_simple.py --image_path $myImgDir --model_name mono+stereo_640x192
+#python test_simple.py --image_path $myImgDir --model_name mono+stereo_640x192
+
+myVideo="$myImgDir.$extension"
+#ffmpeg -i "$myImgDir/"video-frame%05d_disp.jpeg "$myImgDir.$extension"
+ffmpeg  -f image2 -framerate 30 -i "$myImgDir/"video-frame%05d.jpg $myVideo
+echo "=================frames converted to video==================="
+ffmpeg -i $1 -i $myVideo -filter_complex vstack=inputs=2 "$basedir/final.MOV"
+echo "==================videos stacked========================="
